c7bf96b517fb4220669b8da0f8d2700f36d02d7b
Binoculars7 <elitespot.com@gmail.com>
2024-11-20 21:37:33 +0100

    v11

--- controllerUserData.php
+++ controllerUserData.php
@@ -1,15 +1,12 @@
 <?php 
 session_start();
 
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
 
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
-
 
 
 require "connection.php";
